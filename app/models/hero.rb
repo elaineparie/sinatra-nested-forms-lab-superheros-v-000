004187422f8a57291559bd8c0f3fe6a633ebe764
Elaine Anderson <eparie@bu.edu>
2018-07-16 21:04:13 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,7 +1,7 @@
-class Hero
+class Member
   attr_reader :name, :power, :bio
 
-  HEROS = []
+  MEMBERS = []
 
   def initialize(args)
     @name = args[:name]
@@ -11,6 +11,6 @@ class Hero
   end
 
   def self.all
-    HEROS
+    MEMBERS
   end
 end
